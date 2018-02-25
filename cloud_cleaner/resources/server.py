126158bd9b2c42a8af55251b8cee76dd0ebf19e6
Gregory Hellings <greg.hellings@gmail.com>
2018-02-25 00:00:01 -0600

    Handle processing of names
    Properly handle the processing of names, both positive matches and
    negative matches

--- cloud_cleaner/resources/server.py
+++ cloud_cleaner/resources/server.py
@@ -1,7 +1,10 @@
 from cloud_cleaner.config import CloudCleanerConfig, date_format
 from cloud_cleaner.resources.resource import Resource
+from cloud_cleaner.string_matcher import StringMatcher
 from datetime import datetime, timezone
 from munch import Munch
+
+import re
 
 
 class Server(Resource):
@@ -12,6 +15,8 @@ class Server(Resource):
         self.__shade = None
         self.__targets = []
         self._interval = None
+        self.__skip_name = StringMatcher(False)
+        self.__name = StringMatcher(True)
 
     def register(self, config: CloudCleanerConfig):
         """
@@ -21,6 +26,9 @@ class Server(Resource):
         super(Server, self).register(config)
         _desc = "Regex to match the name of the servers"
         self._sub_config.add_argument("--name", "-n", help=_desc)
+        _desc = "Regex to match for servers to ignore"
+        self._sub_config.add_argument("--skip-name", "-s", dest="skip_name",
+                                      help=_desc)
         _desc = "Minimum age (1d, 2w, 6m, 1y)"
         self._sub_config.add_argument("--age", "-a", help=_desc)
 
@@ -38,9 +46,21 @@ class Server(Resource):
         self._config.debug("Found servers: ")
         [self._config.debug("   *** " + t.name) for t in self.__targets]
         # Process for time
+        self._config.info("Parsing dates")
         self._interval = self.parse_interval(self._config.get_arg('age'))
         self.__targets = [t for t in self.__targets if self.__right_age(t)]
         self._config.debug("Parsed ages, servers remaining: ")
+        [self._config.debug("   *** " + t.name) for t in self.__targets]
+        # Process for name
+        self._config.info("Parsing names")
+        skip_name = self._config.get_arg("skip_name")
+        if skip_name is not None:
+            self.__skip_name = re.compile(skip_name)
+        name = self._config.get_arg('name')
+        if name is not None:
+            self.__name = re.compile(name)
+        self.__targets = [t for t in self.__targets if self.__right_name(t)]
+        self._config.debug("Parsed names, servers remaining: ")
         [self._config.debug("   *** " + t.name) for t in self.__targets]
 
     def clean(self):
@@ -55,8 +75,12 @@ class Server(Resource):
         for target in self.__targets:
             self.__shade.delete_server(target.id)
 
-    def __right_age(self, target: Munch):
+    def __right_age(self, target: Munch) -> bool:
         system_age = datetime.strptime(target.created, date_format)
         system_age = system_age.replace(tzinfo=timezone.utc)
         self._config.debug("System %s, age %s" % (target.name, system_age))
         return self._now > (system_age + self._interval)
+
+    def __right_name(self, target: Munch) -> bool:
+        return not self.__skip_name.fullmatch(target.name) and \
+               self.__name.fullmatch(target.name)
---
+++ cloud_cleaner/string_matcher.py
@@ -0,0 +1,13 @@
+class StringMatcher(object):
+    """
+    Used in place where a regex would otherwise be used, but where a constant
+    return value is desired, instead.
+    """
+    def __init__(self, value: bool):
+        self.__value = value
+
+    def match(self, *args) -> bool:
+        return self.__value
+
+    def fullmatch(self, *args) -> bool:
+        return self.__value
