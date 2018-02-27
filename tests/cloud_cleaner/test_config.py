fcc88b6398fea5eaf12ea66682c63c40f2f7eb9b
Gregory Hellings <greg.hellings@gmail.com>
2018-02-26 22:50:00 -0600

    Improve coverage for config.py

--- tests/cloud_cleaner/test_config.py
+++ tests/cloud_cleaner/test_config.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from cloud_cleaner.config import CloudCleanerConfig
 from argparse import ArgumentParser
 from logging import getLogger, WARNING, INFO, DEBUG
+from shade import OpenStackCloud
 
 
 class TestConfig(TestCase):
@@ -12,12 +13,15 @@ class TestConfig(TestCase):
         config.set_args(["--os-auth-url", "http://no.com", "item"])
         config.parse_args()
         self.assertEqual("item", config.get_resource())
+        config.warning("Dummy warning")
         log = getLogger("cloud_cleaner")
         self.assertEqual(log.getEffectiveLevel(), WARNING)
+        self.assertIsNone(config.get_arg("no_arg"))
 
     def test_subcommand_incorrect(self):
         parser = ArgumentParser()
         config = CloudCleanerConfig(parser=parser, args=["notfound"])
+        config.set_args(["notfound"])
         config.add_subparser("found")
         # ArgumentError raises SystemExit internally, apparently
         with self.assertRaises(SystemExit):
@@ -34,6 +38,8 @@ class TestConfig(TestCase):
         config.parse_args()
         log = getLogger("cloud_cleaner")
         self.assertEqual(log.getEffectiveLevel(), INFO)
+        shade = config.get_shade()
+        self.assertIsInstance(shade, OpenStackCloud)
 
     def test_verbose_two(self):
         args = ["--os-auth-url", "http://no.com", "-vv"]
