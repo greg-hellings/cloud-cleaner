9e928d703a022fc1b05411b532764600369bd92d
Gregory Hellings <greg.hellings@gmail.com>
2018-02-28 13:48:02 -0600

    Bump version to 0.1.0
    Prepare for 0.1.0 release
    Add CHANGELOG

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cloud-cleaner',
-    version='0.0.2',
+    version='0.1.0',
     license='BSD',
     author='Gregory Hellings',
     author_email='greg.hellings@gmail.com',
